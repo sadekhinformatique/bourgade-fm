d3146f09931fa1621ac7bd8887549f88b54ce563
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2026-02-17 16:21:21 +0100

    fix: Update Bourgade FM stream URL
    Switches the stream URL to a new HLS manifest for improved compatibility and reliability.

--- constants.ts
+++ constants.ts
@@ -11,7 +11,7 @@ export interface ExtendedRadioInfo extends RadioInfo {
 export const RADIO_DATA: ExtendedRadioInfo = {
   name: "Bourgade FM",
   logo: "https://cdn.onlineradiobox.com/img/l/4/152174.v1.png",
-  streamUrl: "https://radioburkinafm.ice.infomaniak.ch/radioburkinafm-128.mp3",
+  streamUrl: "https://radioburkinafm.radiohls.infomaniak.com/radioburkinafm/manifest.m3u8",
   description: "La radio des bourgs, faubourgs et bourgades. Une station patriote et professionnelle au cœur du Burkina Faso.",
   frequency: "94.3 MHz",
   slogan: "C'est l'auditoire qui décide !",
