a3cc7c69490d43fc526065d3c9e7f0ef6cb813d5
sadekhinformatique <155910956+sadekhinformatique@users.noreply.github.com>
2026-02-22 14:17:00 +0100

    Update streamUrl for Bourgade FM

--- constants.ts
+++ constants.ts
@@ -11,7 +11,7 @@ export interface ExtendedRadioInfo extends RadioInfo {
 export const RADIO_DATA: ExtendedRadioInfo = {
   name: "Bourgade FM",
   logo: "https://cdn.onlineradiobox.com/img/l/4/152174.v1.png",
-  streamUrl: "https://radioburkinafm.radiohls.infomaniak.com/radioburkinafm/manifest.m3u8",
+  streamUrl: "https://radiobourgadefm.ice.infomaniak.ch/radiobourgadefm-96.mp3",
   description: "La radio des bourgs, faubourgs et bourgades. Une station patriote et professionnelle au cœur du Burkina Faso.",
   frequency: "94.3 MHz",
   slogan: "C'est l'auditoire qui décide !",
